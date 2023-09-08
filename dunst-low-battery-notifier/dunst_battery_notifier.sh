c4be93a2fb8a9e29a223ad63d9094d63e737db92
Michał Sabiniarz <michal.sabiniarz@gmail.com>
2023-09-08 18:11:51 +0200

    Add below 20% message to charge the battery don't display the message if battery is charging.

--- dunst-low-battery-notifier/dunst_battery_notifier.sh
+++ dunst-low-battery-notifier/dunst_battery_notifier.sh
@@ -2,7 +2,13 @@
 
 ## Get battery percentage
 cap=$(cat /sys/class/power_supply/BAT0/capacity)
+status=$(cat /sys/class/power_supply/BAT0/status)
 
-if [ $cap -lt 10 ]; then
-    dunstify -u critical "Battery is low" "Battery is at ${cap}%"
+if [[ $cap -lt 10 && $status == "Discharging" ]]; then
+    dunstify -u critical "Battery is extremely low. Please plug in the charger." "Battery is at ${cap}%"
 fi
+
+if [[ $cap -lt 20 && $status == "Discharging" ]]; then
+    dunstify -u normal "Battery is low. Please plug in the charger." "Battery is at ${cap}%"
+fi
+
